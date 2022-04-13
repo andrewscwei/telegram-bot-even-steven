fd562cfe0a394b5a9a98101560aa633f849637e7
Andrew Wei <andrewscwei@gmail.com>
2022-04-13 12:30:51 -0700

    Remove extraneous code

--- app/utils/send_message.py
+++ app/utils/send_message.py
@@ -1,14 +1,6 @@
-import logging
 import os
 
 import requests
-
-logging.basicConfig(
-  level=logging.DEBUG,
-  format='%(asctime)s <%(name)s> [%(levelname)s] %(message)s',
-)
-
-logger = logging.getLogger("bot")
 
 def send_message(chat_id: str, text: str) -> None:
   token = os.environ.get("TELEGRAM_BOT_TOKEN")
