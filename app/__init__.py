3eeb4ba04cef0652fd7a84552fd094b9df833fdf
Andrew Wei <andrewscwei@gmail.com>
2022-04-16 10:50:09 -0700

    Fix db connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from .bot import dispatcher
 from .db import init_db
 from .routes import routes
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,12 @@
 import os
 
-def test_token():
+from app import dispatcher
+
+
+def test_bot_token():
   assert 'BOT_TOKEN' in os.environ
+
+def test_bot_dispatcher():
+  assert dispatcher is not None
+  assert dispatcher.bot is not None
+  assert dispatcher.bot.token is not None
