e29c2983d543c2276ed51073ddebf0fa32b2ceca
Andrew Wei <andrewscwei@gmail.com>
2022-04-17 12:13:07 -0700

    Refactor remove command

--- app/commands/remove.py
+++ app/commands/remove.py
@@ -1,9 +1,9 @@
-from telegram import ParseMode, Update
+from telegram import Update
 from telegram.ext import CallbackContext
 
 from ..db import db
 from ..models import Expense
-from ..utils import format_currency, log, parse_int
+from ..utils import format_currency, parse_int
 
 
 def remove(update: Update, context: CallbackContext):
@@ -12,20 +12,13 @@ def remove(update: Update, context: CallbackContext):
   try:
     expense_id = parse_int(context.args[0])
   except Exception as exc:
-    log.exception('Parsing ID... %s: %s', 'ERR', exc)
-
-    return update.message.reply_text(
-      '💩 Looks like you didn\'t provide a valid expense ID (example: `/remove <expense_id>`)',
-      parse_mode=ParseMode.MARKDOWN,
-      quote=False,
-    )
+    raise Exception('💩 Looks like you didn\'t provide a valid expense ID (example: `/remove <expense_id>`)') from exc
 
   expense = Expense.query.filter_by(id=expense_id, chat_id=chat_id).first()
 
   if expense is None:
-    return update.message.reply_text(
+    return update.message.reply_markdown(
       f'✋ No expense found with ID {expense_id}',
-      parse_mode=ParseMode.MARKDOWN,
       quote=True,
     )
 
@@ -34,11 +27,14 @@ def remove(update: Update, context: CallbackContext):
   if expense.label.strip():
     reply += f': {expense.label}'
 
-  db.session.delete(expense)
-  db.session.commit()
+  try:
+    db.session.delete(expense)
+    db.session.commit()
+  except Exception as exc:
+    db.session.rollback()
+    raise exc
 
-  update.message.reply_text(
+  update.message.reply_markdown(
     reply,
-    parse_mode=ParseMode.MARKDOWN,
     quote=False,
   )
