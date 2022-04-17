d1dd7c669dec458430608d07e5ae13e4363f7041
Andrew Wei <andrewscwei@gmail.com>
2022-04-17 11:44:21 -0700

    Improve error handling

--- app/commands/error.py
+++ app/commands/error.py
@@ -9,12 +9,11 @@ def error(update: Update, context: CallbackContext):
   chat_id = update.message.chat.id
   text = update.message.text
   exc = context.error
+  reply = str(exc) if str(exc).strip() else '💩 Something went wrong, please try again later'
 
-  log.exception('Handling message "%s" for chat ID <%s>... ERR: %s', text, chat_id, exc)
+  log.exception('Handling message "%s" for chat ID <%s>... ERR', text, chat_id)
 
-  db.session.rollback()
-
-  update.message.reply_markdown_v2(
-    '💩 Something went wrong, please try again later',
+  update.message.reply_markdown(
+    reply,
     quote=False,
   )
