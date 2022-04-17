c20d22e2563ccada74374cef175ddea8b2ccfd92
Andrew Wei <andrewscwei@gmail.com>
2022-04-17 11:44:25 -0700

    Refactor add command

--- app/commands/add.py
+++ app/commands/add.py
@@ -1,9 +1,9 @@
-from telegram import ParseMode, Update
+from telegram import Update
 from telegram.ext import CallbackContext
 
 from ..db import db
 from ..models import Expense
-from ..utils import format_currency, log, parse_float
+from ..utils import format_currency, parse_float
 
 
 def add(update: Update, context: CallbackContext):
@@ -13,28 +13,24 @@ def add(update: Update, context: CallbackContext):
   try:
     amount = parse_float(context.args[0])
   except Exception as exc:
-    log.exception('Parsing amount... %s: %s', 'ERR', exc)
-
-    return update.message.reply_text(
-      '💩 Looks like you didn\'t provide a valid amount (example: `/add 99.99 <optional_label>`)',
-      parse_mode=ParseMode.MARKDOWN,
-      quote=False,
-    )
+    raise Exception('💩 Looks like you didn\'t provide a valid amount (example: `/add 99.99 <optional_label>`)') from exc
 
   label = ' '.join(context.args[1:])
-
   expense = Expense(chat_id=chat_id, user=user, amount=amount, label=label)
 
-  db.session.add(expense)
-  db.session.commit()
+  try:
+    db.session.add(expense)
+    db.session.commit()
+  except Exception as exc:
+    db.session.rollback()
+    raise exc
 
   reply = f'👌 Added `{format_currency(amount)}` for @{user}'
 
   if label.strip():
     reply += f': {label}'
 
-  update.message.reply_text(
+  update.message.reply_markdown(
     reply,
-    parse_mode=ParseMode.MARKDOWN,
     quote=False,
   )
