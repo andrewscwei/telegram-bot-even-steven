b3d23cda89dfdbe2a051e30d4729aaccb1f3913a
Andrew Wei <andrewscwei@gmail.com>
2022-04-17 11:27:51 -0700

    Better logs for error handler

--- app/commands/error.py
+++ app/commands/error.py
@@ -1,14 +1,20 @@
-from telegram import ParseMode, Update
+from telegram import Update
 from telegram.ext import CallbackContext
 
 from ..db import db
+from ..utils import log
 
 
 def error(update: Update, context: CallbackContext):
+  chat_id = update.message.chat.id
+  text = update.message.text
+  exc = context.error
+
+  log.exception('Handling message "%s" for chat ID <%s>... ERR: %s', text, chat_id, exc)
+
   db.session.rollback()
 
-  update.message.reply_text(
+  update.message.reply_markdown_v2(
     '💩 Something went wrong, please try again later',
-    parse_mode=ParseMode.MARKDOWN,
     quote=False,
   )
