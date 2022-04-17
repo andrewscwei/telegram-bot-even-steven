a10f0b1296417a56ba3b1ce77be74af01bd1de4c
Andrew Wei <andrewscwei@gmail.com>
2022-04-17 12:29:36 -0700

    Update version formatting

--- app/commands/version.py
+++ app/commands/version.py
@@ -6,6 +6,6 @@ from config import BUILD_NUMBER, VERSION
 
 def version(update: Update, context: CallbackContext):
   update.message.reply_markdown(
-    f'🤖 I\'m running on {VERSION}-{BUILD_NUMBER}',
+    f'🤖 I\'m running on `{VERSION}-{BUILD_NUMBER}`',
     quote=False,
   )
