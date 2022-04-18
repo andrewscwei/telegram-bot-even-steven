646d16b1687a693cc9c2f910547658659e47b100
Andrew Wei <andrewscwei@gmail.com>
2022-04-18 16:07:38 -0700

    Add parse_user util function

--- app/utils/parsers.py
+++ app/utils/parsers.py
@@ -1,3 +1,6 @@
+from telegram import User
+
+
 def parse_float(value) -> float:
   ret = float(value)
   return ret
@@ -5,3 +8,8 @@ def parse_float(value) -> float:
 def parse_int(value) -> int:
   ret = int(value)
   return ret
+
+def parse_user(user: User) -> str:
+  username = user.username
+  first_name = user.first_name
+  return f'@{username}' if username.strip() else first_name
